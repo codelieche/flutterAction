914cedf4bf6fee8ebc4bf9f0c3ae2bea48e686f6
codelieche <codelieche@gmail.com>
2020-09-10 19:21:06 +0800

    add scaffold bottomnavigationbar demo01

---
+++ tutorial/lib/app/pages/widget/scaffold/bottomNavigationBar/demo01.dart
@@ -0,0 +1,103 @@
+// Scaffold的bottomNavigationBar
+// 常见的属性
+// items: List<BottomNavigationBarItem> 底部导航条按钮集合
+// iconSize：icon的大小
+// currentIndex: 默认选中第几个
+// onTap：选中变化回调函数
+// fixedColor: 选中颜色
+// type：BottomNavigationBarType.fixed/shifting
+import 'package:flutter/material.dart';
+
+class BottomNavigationBarDemoPage01 extends StatefulWidget {
+  BottomNavigationBarDemoPage01({Key key}) : super(key: key);
+
+  @override
+  _BottomNavigationBarDemoPage01State createState() =>
+      _BottomNavigationBarDemoPage01State();
+}
+
+class _BottomNavigationBarDemoPage01State
+    extends State<BottomNavigationBarDemoPage01> {
+  int _currentBarIndex = 0; // 当前选中的导航bar
+  List<Widget> _bodyPages = []; // 导航bar对应的页面
+
+  @override
+  void initState() {
+    super.initState();
+
+    // app的初始页面
+    _bodyPages = [
+      Scaffold(
+        appBar: AppBar(
+          title: Text("首页"),
+        ),
+        body: Center(
+          child: Text("Hello index Page"),
+        ),
+      ),
+      Scaffold(
+        appBar: AppBar(
+          title: Text("菜单页"),
+        ),
+        body: Center(
+          child: Text("Hello menu Page"),
+        ),
+      ),
+      Scaffold(
+        appBar: AppBar(
+          title: Text("设置页"),
+        ),
+        body: Center(
+          child: Text("Hello settings Page"),
+        ),
+      ),
+      Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.pinkAccent,
+          title: Text("用户页"),
+        ),
+        body: Center(
+          child: Text("Hello user Page"),
+        ),
+      ),
+    ];
+  }
+
+  Widget build(BuildContext context) {
+    // 底部导航
+    BottomNavigationBar appNavigationBar = BottomNavigationBar(
+      currentIndex: _currentBarIndex, // 当前选中的bar
+      items: [
+        // 底部导航的tab选项
+        BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("首页")),
+        BottomNavigationBarItem(icon: Icon(Icons.menu), title: Text("菜单")),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text("设置")),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.account_circle),
+          title: Text("用户"),
+        ),
+      ],
+      onTap: (index) {
+        // 切换底部导航
+        setState(() {
+          // 设置选中的导航bar
+          _currentBarIndex = index;
+        });
+      },
+      iconSize: 24.0, // 图标icon的大小,
+      selectedFontSize: 12, // 选中的文字大小，默认是14
+      unselectedFontSize: 12, // 为选中的文字大小，默认是12
+      fixedColor: Colors.pinkAccent, // 选中的bar颜色
+      type: BottomNavigationBarType.fixed, // 配置底部bars可以有多个按钮
+    );
+    // 脚手架
+    Scaffold scaffold = Scaffold(
+      // appBar: AppBar(
+      //   title: Text("底部导航"),
+      // ),
+      body: _bodyPages[_currentBarIndex], // 主体内容
+      bottomNavigationBar: appNavigationBar, // 底部导航
+    );
+    return scaffold;
+  }
+}
--- tutorial/lib/app/pages/widget/scaffold/index.dart
+++ tutorial/lib/app/pages/widget/scaffold/index.dart
@@ -1,8 +1,10 @@
-/**
- * Scaffold相关的组件示例
- * 路由：/widget/scaffold/index
- */
+//  Scaffold相关的组件示例
+//  路由：/widget/scaffold/index
 import 'package:flutter/material.dart';
+import 'package:tutorial/app/components/items/title.dart';
+
+// 底部导航示例：
+import 'bottomNavigationBar/demo01.dart';
 
 class ScaffoldWidgetIndexPage extends StatefulWidget {
   @override
@@ -13,11 +15,40 @@ class ScaffoldWidgetIndexPage extends StatefulWidget {
 class _ScaffoldWidgetIndexPageState extends State<ScaffoldWidgetIndexPage> {
   @override
   Widget build(BuildContext context) {
+    // 页面主体内容
+    Widget body = ListView(
+      children: [
+        // appBar
+        ListTile(
+          leading: Icon(Icons.arrow_right),
+          title: Text("appBar"),
+          onTap: () {
+            print("app bar");
+          },
+        ),
+        Divider(),
+        // bottomNavigationBar
+        SimpleTitleWidget(
+          title: "底部导航",
+          description: "bottomNavigationBar",
+        ),
+        ListTile(
+          leading: Icon(Icons.arrow_right),
+          title: Text("bottomNavigationBar:示例01"),
+          onTap: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (BuildContext ctx) {
+              return BottomNavigationBarDemoPage01();
+            }));
+          },
+        ),
+      ],
+    );
     Scaffold scaffold = Scaffold(
       appBar: AppBar(
         title: Text("Scaffold"),
       ),
-      body: Text("scaffold"),
+      body: body,
     );
 
     return scaffold;
